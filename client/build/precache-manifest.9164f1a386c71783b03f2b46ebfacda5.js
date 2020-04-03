ee39fac56c5097b9e05ed4020127861cf97fa6d8
Jesse McKinney <jessman51386@gmail.com>
2020-04-02 23:07:25 -0400

    yay now registered, I think?

---
+++ client/build/precache-manifest.9164f1a386c71783b03f2b46ebfacda5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28633aade7743526bf22f388c7a858b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1f2bd5bab0d12fd9d3",
+    "url": "/static/css/main.28fae4b3.chunk.css"
+  },
+  {
+    "revision": "c638031937cf83fe1999",
+    "url": "/static/js/2.0ebfd93e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0ebfd93e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1f2bd5bab0d12fd9d3",
+    "url": "/static/js/main.973733c7.chunk.js"
+  },
+  {
+    "revision": "fe89dde6e011bbc6ffe5",
+    "url": "/static/js/runtime-main.b81046d5.js"
+  }
+]);
